f5b85378d6e9e63bcd6643e7023074879b481ab4
Artyom <ptukhovAN@gmail.com>
2023-08-17 13:01:46 +0500

    feat: add base structure , UI and logics

---
+++ src/app/App.tsx
@@ -0,0 +1,18 @@
+import s from './App.module.scss';
+import {AddContactForm} from '../components/AddContactForm/AddContactForm';
+import {UITable} from '../components/Table/Table';
+import {Route, Routes} from 'react-router-dom';
+
+function App() {
+	return (
+		<div className={s.appWrap}>
+			<Routes>
+				<Route path="/" element={<UITable />} />
+				<Route path="/new-contact" element={<AddContactForm />} />
+				<Route path="/*" element={<div>error!</div>} />
+			</Routes>
+		</div>
+	);
+}
+
+export default App;
---
+++ src/store/store.ts
@@ -0,0 +1,13 @@
+import {AnyAction, combineReducers, configureStore, ThunkDispatch,} from '@reduxjs/toolkit';
+import {userReducer} from './reducers/users-reduser';
+
+const rootReducer = combineReducers({
+	usersSlice: userReducer,
+});
+
+export const store = configureStore({
+	reducer: rootReducer,
+});
+
+export type AppStoreType = ReturnType<typeof rootReducer>;
+export type AppDispatchType = ThunkDispatch<AppStoreType, void, AnyAction>;
